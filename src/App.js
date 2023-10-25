808357c20579bc0cbfa1dc065c871eb74aae0f7a
dineshkumar-stack <stardinesh4@gmail.com>
2023-10-25 14:04:14 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mainbar from './Components/Mainbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Mainbar />
+    </div>
+  );
+}
+
+export default App;
